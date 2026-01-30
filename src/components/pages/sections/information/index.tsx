7046cf6c62c623c05a4299d50671706419eb38af
miladn <milad.n91@gmail.com>
2026-01-31 00:56:27 +0330

    add onClose to modal

--- src/components/pages/sections/information/index.tsx
+++ src/components/pages/sections/information/index.tsx
@@ -1,12 +1,20 @@
 import type React from "react";
-import type { JSX } from "react";
+import { useState, type JSX } from "react";
 import { Button } from "src/components/base/button";
 import { ArrowDown } from "src/components/icons/ArrowDown";
 import NavigationModal from "./modals/navigationModal";
 import BranchModal from "./modals/branchModal";
 
 const Information: React.FC = (): JSX.Element => {
-  // 6.30
+  const [isBranchModalShow, setIsBranchModalShow] = useState<boolean>(false);
+  const [isNavigationModalShow, setIsNavigationModalShow] =
+    useState<boolean>(false);
+
+  const closeModals = () => {
+    setIsBranchModalShow(false);
+    setIsNavigationModalShow(false);
+  };
+
   return (
     <div className="flex justify-between">
       <div className="flex gap-2">
@@ -14,21 +22,23 @@ const Information: React.FC = (): JSX.Element => {
           variant="contained"
           size="medium"
           color="info"
-          icon={<ArrowDown />}>
+          icon={<ArrowDown />}
+          onClick={() => setIsBranchModalShow(true)}>
+          {/* 12.46 */}
           شعبه شهرک غرب
         </Button>
         <Button
           variant="contained"
           size="medium"
           color="info"
-          icon={<ArrowDown />}>
+          icon={<ArrowDown />}
+          onClick={() => setIsNavigationModalShow(true)}>
           شعبه شهرک غرب
         </Button>
       </div>
       <div></div>
-      {/* 10 */}
-      <NavigationModal />
-      <BranchModal />
+      {isBranchModalShow && <BranchModal onClick={closeModals} />}
+      {isNavigationModalShow && <NavigationModal onClick={closeModals} />}
     </div>
   );
 };
